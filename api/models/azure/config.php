b44138e05e2278ddefc1f5e19ba71e8356c6c530
stacywaturi <stacy.waturi@gmail.com>
2019-02-06 12:04:44 +0200

    Initial
    Create key (API)
    Insert created key into DB

--- api/models/azure/config.php
+++ api/models/azure/config.php
@@ -8,11 +8,11 @@
 namespace Azure;
 
 class Config {
-    static $APP_TENANT_ID   = 'azureisolvtech.onmicrosoft.com';
-    static $CLIENT_ID       = '9d98c3f3-78fa-4449-ae07-6a01f58a8446';
-    static $USERNAME        = 'stacy@isolvtech.com';
-    static $PASSWORD        = 'GodRules100';
-    static $KEY_VAULT_NAME  = 'tf-test-vault';
+    static $APP_TENANT_ID   = '';
+    static $CLIENT_ID       = '';
+    static $USERNAME        = '';
+    static $PASSWORD        = '';
+    static $KEY_VAULT_NAME  = '';
 
 }
 
